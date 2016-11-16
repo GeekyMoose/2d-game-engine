76e6c7ad7861ffcc150791ac136164625f1f187d
geekymoose <constantin.masson@umontreal.ca>
2016-11-15 20:14:39 -0500

    Add camera

---
+++ sdltutorials/Camera.cpp
@@ -0,0 +1,53 @@
+#include "Camera.h"
+
+Camera Camera::cameraControl;
+
+Camera::Camera(){
+	x = 0;
+	y = 0;
+	targetX = NULL;
+	targetY = NULL;
+	targetMode = TARGET_MODE_NORMAL;
+}
+
+Camera::~Camera(){
+}
+
+void Camera::moveCamera(int moveX, int moveY){
+	x += moveX;
+	y += moveY;
+}
+
+int Camera::getX(){
+	//If no target, return current position
+	if(targetX==NULL){
+		return x;
+	}
+	//Here, means target set, return position according to camera mode
+	if(targetMode==TARGET_MODE_CENTER){
+		return *targetX-(SCREEN_WIDTH/2);
+	}
+	return *targetX;
+}
+
+int Camera::getY(){
+	//If no target, return current position
+	if(targetY==NULL){
+		return y;
+	}
+	//Here, means target set, return position according to camera mode
+	if(targetMode==TARGET_MODE_CENTER){
+		return *targetX-(SCREEN_HEIGHT/2);
+	}
+	return *targetY;
+}
+
+void Camera::setPosition(int posX, int posY){
+	x = posX;
+	y = posY;
+}
+
+void Camera::setTarget(int *posX, int* posY){
+	targetX = posX;
+	targetY = posY;
+}
---
+++ sdltutorials/Camera.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <SDL.h>
+
+#include "constants.h"
+
+enum{
+	TARGET_MODE_NORMAL = 0, //Position to the top left of the target
+	TARGET_MODE_CENTER //Center camera to the target
+};
+
+class Camera{
+	//--------------------------------------------------------------------------
+	// Attributes
+	//--------------------------------------------------------------------------
+public:
+	static Camera cameraControl; //To access camera from anywhere
+private:
+	int x; //Camera position (in pixel)
+	int y;
+	int* targetX; //To target an element (Null if not target)
+	int* targetY;
+public:
+	int targetMode;
+
+public:
+	//--------------------------------------------------------------------------
+	// Constructors
+	//--------------------------------------------------------------------------
+	Camera();
+	~Camera();
+
+	//--------------------------------------------------------------------------
+	// Body functions
+	//--------------------------------------------------------------------------
+
+	/**
+	 * \brief Move the camera
+	 * 
+	 * \param moveX X coordinate translation
+	 * \param moveY Y coordinate translation
+	 */
+	void moveCamera(int moveX, int moveY);
+
+	/**
+	 * \brief Return X camera position
+	 * \details The returned value depend if a target is set and use the camera mode.
+	 *
+	 * \return Current X coordinate
+	 */
+	int getX();
+
+	/**
+	* \brief Return Y camera position
+	* \details The returned value depend if a target is set and use the camera mode.
+	*
+	* \return Current Y coordinate
+	*/
+	int getY();
+
+	void setPosition(int posX, int posY);
+	void setTarget(int* posX, int* posY);
+};
+
