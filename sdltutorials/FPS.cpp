41e08a561da257e453b7f0cacca6da0fcb069c42
geekymoose <constantin.masson@umontreal.ca>
2016-12-10 18:31:45 -0500

    Add FPS class for Frames management

---
+++ sdltutorials/FPS.cpp
@@ -0,0 +1,32 @@
+#include "FPS.h"
+
+FPS FPS::FPSControl;
+
+FPS::FPS(){
+	oldTime		= 0;
+	lastTime	= 0;
+	speedFactor = 0;
+	nbFrames	= 0;
+	fpsCounter	= 0;
+}
+
+void FPS::onLoop(){
+	//If one second since last refresh, update fps data
+	if(oldTime+1000>SDL_GetTicks()){
+		oldTime		= SDL_GetTicks();
+		nbFrames	= fpsCounter;
+		fpsCounter	= 0;
+	}
+	//Update speedFactor and frames.
+	speedFactor = ((SDL_GetTicks()-lastTime)/1000.0f)*32.0f; //32.0 is arbitrary
+	lastTime	= SDL_GetTicks();
+	fpsCounter++;
+}
+
+int FPS::getFPS(){
+	return nbFrames;
+}
+
+float FPS::getSpeedFactor(){
+	return speedFactor;
+}
